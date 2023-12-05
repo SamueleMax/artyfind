4d988c7b71ab926d4982dd53ef3cb9655583d06d
Samuele Max <max.samuele@itiscannizzaro.edu.it>
2023-12-05 18:44:18 +0100

    Add icons

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
         <title>Artyfind</title>
     </head>
     <body>
@@ -19,18 +20,18 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/index.php">Home</a>
+                                <a class="nav-link active" aria-current="page" href="/index.php"><i class="bi bi-house-fill"></i> Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/map.php">Mappa</a>
+                                <a class="nav-link" href="/map.php"><i class="bi bi-geo-alt-fill"></i> Mappa</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/profile.php">Profilo</a>
+                                <a class="nav-link" href="/profile.php"><i class="bi bi-person-fill"></i> Profilo</a>
                             </li>
                         </ul>
                         <form class="d-flex" role="search">
                             <input class="form-control me-2" type="search" placeholder="Cerca" arial-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Cerca</button>
+                            <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
                         </form>
                     </div>
                 </div>
@@ -38,7 +39,7 @@
 
             <div class="container mt-4">
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newPostModal">
-                    Nuovo post
+                    <i class="bi bi-plus"></i> Nuovo post
                 </button>
             </div>
         </header>
